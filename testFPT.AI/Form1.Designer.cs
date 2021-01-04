2b014e94a8ccf11625bd7a0d53fbbb185bd1119e
The Anh <caotheanh.buh@outlook.com>
2021-01-04 23:35:43 +0700

    Add project files.

---
+++ testFPT.AI/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace testFPT.AI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_play
+            // 
+            this.btn_play.Location = new System.Drawing.Point(294, 510);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(159, 48);
+            this.btn_play.TabIndex = 0;
+            this.btn_play.Text = "Play";
+            this.btn_play.UseVisualStyleBackColor = true;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(744, 510);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(159, 48);
+            this.btn_exit.TabIndex = 0;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(176, 12);
+            this.txt_input.Multiline = true;
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(884, 304);
+            this.txt_input.TabIndex = 1;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(176, 316);
+            this.progressBar.Minimum = 1;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(884, 12);
+            this.progressBar.TabIndex = 2;
+            this.progressBar.Value = 1;
+            this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
+            // 
+            // txt_url
+            // 
+            this.txt_url.Location = new System.Drawing.Point(176, 406);
+            this.txt_url.Multiline = true;
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(884, 68);
+            this.txt_url.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 628);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.txt_url);
+            this.Controls.Add(this.txt_input);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_play);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text to void";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox txt_url;
+    }
+}
+
---
+++ testFPT.AI/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using WMPLib;
+using System.Net;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+
+
+namespace testFPT.AI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // chuyển txt ==> âm thanh 
+        public string amthanh()
+        {
+            String result = Task.Run(async () =>
+            {
+
+                String payload =txt_input.Text;
+               
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("api-key", "vSZGBnEDWIxL38x0gGdnnzcY4Q0miuPn");
+                client.DefaultRequestHeaders.Add("speed", "");
+                client.DefaultRequestHeaders.Add("voice", "banmai");
+                var response = await client.PostAsync("https://api.fpt.ai/hmi/tts/v5", new StringContent(payload));
+                return await response.Content.ReadAsStringAsync();
+            }).GetAwaiter().GetResult();
+
+            string[] str = result.Split('"');
+            return str[3];
+
+        }
+
+        //tải file mp3 vào thư mục sound 
+        public int taifile()
+        {
+            txt_url.Text = amthanh();
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFileAsync(
+                    // Param1 = Link of file
+                    new System.Uri(amthanh()),
+                    // param2 = path to save
+                    @"C:\Users\Cao Anh\OneDrive - st.buh.edu.vn\Lập trình hướng đối tượng\testFPT.AI\testFPT.AI\sound\11.mp3"
+                );
+                return (1);
+            }
+        }
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            if (taifile() == 1)
+            {
+
+                WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+
+                wplayer.URL = amthanh();
+                //@"C:\Users\Cao Anh\OneDrive - st.buh.edu.vn\Lập trình hướng đối tượng\testFPT.AI\testFPT.AI\sound\11.mp3";
+                wplayer.controls.play();
+            }
+
+        }
+
+        private void progressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            txt_url.Text = amthanh();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            DialogResult re = MessageBox.Show("Bạn có muốn thoát?", "Xác nhận", MessageBoxButtons.YesNo);
+            if (re == DialogResult.Yes) Close();
+
+        }
+    }
+}
